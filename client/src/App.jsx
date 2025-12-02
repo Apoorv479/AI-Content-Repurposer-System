610adc328a0e6cfb7acc8ddadea0245eb456f640
Apoorv <apoorv172900@gmail.com>
2025-12-03 01:38:25 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-7xl bg-amber-900 text-amber-500'>Hello World....</h1>
+    </div>
+  )
+}
+
+export default App
